8f2b00aec6f4dae8148f85844a057fcb9169adc9
onfarooq0008-eng <onfarooq0008@gmail.com>
2026-07-19 19:02:50 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ set -e
 
 export DISPLAY=:1
 
-Xvfb :1 -screen 0 1280x720x16 &
+Xvfb :1 -screen 0 1280x720x16 -ac &
 
 sleep 2
 
@@ -11,17 +11,22 @@ firefox-esr \
   --no-remote \
   --new-instance \
   --kiosk \
-  --private-window about:blank \
-  --safe-mode &
+  --private-window \
+  --safe-mode about:blank &
 
 x11vnc \
   -display :1 \
   -forever \
   -shared \
   -nopw \
+  -noxrecord \
+  -noxfixes \
+  -noxdamage \
   -rfbport 5900 &
 
 websockify \
   --web=/usr/share/novnc \
   8080 \
   localhost:5900
+
+echo "Firefox RDP ready"
